1d8c674c2a20428224559f86301c915f980779d3
Cloudy Young <cloudyyoung74@outlook.com>
2020-01-07 02:02:36 -0700

    Initial
    Love Spectre CSS

---
+++ view/sso/_signin_header.php
@@ -0,0 +1,39 @@
+<meta charset="utf-8">
+<title></title>
+
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Security-Policy" content="">
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://unpkg.com/jquery"></script>
+<script>
+  if (typeof module === 'object') {
+    window.jQuery = window.$ = module.exports;
+  };
+</script>
+
+
+<!--SPECTRE-->
+<link href="/css/spectre.min.css" rel="stylesheet">
+<link href="/css/spectre-exp.min.css" rel="stylesheet">
+<link href="/css/spectre-icons.min.css" rel="stylesheet">
+<link href="/css/signin.css" rel="stylesheet">
+<link href="https://unpkg.com/animate.css" rel="stylesheet">
+
+
+<!--I18N LANGUAGE-->
+<script src="https://unpkg.com/jquery-i18n-properties"></script>
+<script>
+  $.i18n.properties({
+    name: ['lang', 'lang_sso_request', 'lang_app'],
+    path: '/lang/',
+    mode: 'map',
+    cache: false,
+    encoding: 'UTF-8'
+  });
+</script>
+
+
+<!--JS HEAD-->
+<script src="/js/js-head.js"></script>
---
+++ view/users/_accounts_header.php
@@ -0,0 +1,38 @@
+<meta charset="utf-8">
+<title></title>
+
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Security-Policy" content="">
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://unpkg.com/jquery"></script>
+<script>
+  if (typeof module === 'object') {
+    window.jQuery = window.$ = module.exports;
+  };
+</script>
+
+
+<!--SPECTRE-->
+<link href="/css/spectre.min.css" rel="stylesheet">
+<link href="/css/spectre-exp.min.css" rel="stylesheet">
+<link href="/css/spectre-icons.min.css" rel="stylesheet">
+<link href="/css/account.css" rel="stylesheet">
+<link href="https://unpkg.com/animate.css" rel="stylesheet">
+
+<!--I18N LANGUAGE-->
+<script src="https://unpkg.com/jquery-i18n-properties"></script>
+<script>
+  $.i18n.properties({
+    name: ['lang'],
+    path: '/lang/',
+    mode: 'map',
+    cache: false,
+    encoding: 'UTF-8'
+  });
+</script>
+
+
+<!--JS HEAD-->
+<script src="/js/js-head.js"></script>
